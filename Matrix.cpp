37d6b5b548db44156e9ed6f33f6a81d727b4b101
allie1587 <130801516+allie1587@users.noreply.github.com>
2023-10-18 13:29:00 -0500

    upload lab06

---
+++ Matrix.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <fstream>
+#include "stdio.h"
+
+const int MAX_SIZE = 100;
+
+int main(){
+    //I'm messing something up in the reading the file
+    char filename[100];
+    std::cout << "Enter the file name: ";
+    std::cin >> filename;
+    std::ifstream file(filename);
+    int size;
+    // Read the size of the square matrix from the first line
+    file >> size;
+    int matrixA[MAX_SIZE][MAX_SIZE];
+    int matrixB[MAX_SIZE][MAX_SIZE];
+    // Read matrix elements into the 2D array
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            file >> matrixA[i][j];
+        }
+    }
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            file >> matrixB[i][j];
+        }   
+    }
+    file.close();
+    std::cout << "\nMatrix A:\n";
+    for (int i =0; i < size; i++){
+        for (int j=0; j < size; j++){
+            std::cout<< matrixA[i][j] << " ";
+        }
+        std::cout << "\n";
+    }
+    std::cout << "Matrix B:\n";
+    for (int i =0; i < size; i++){
+        for (int j=0; j < size; j++){
+            std::cout<< matrixB[i][j] << " ";
+        }
+        std::cout << "\n";
+    }
+    std::cout << "Matrix Sum (A + B):\n";
+    int matrixSum[MAX_SIZE][MAX_SIZE];
+    for(int i=0; i < size; i++){
+        for (int j=0; j < size; j++){
+            matrixSum[i][j] = matrixA[i][j] + matrixB[i][j];
+        }
+    }
+    for (int i =0; i < size; i++){
+        for (int j=0; j < size; j++){
+            // std::cout<< matrixSum[i][j] << " ";
+            printf("%2d ", matrixSum[i][j]);
+        }
+        std::cout << "\n";
+    }
+    std::cout << "Matrix Product (A * B):\n";
+    int matrixProduct[MAX_SIZE][MAX_SIZE];
+    for (int i=0; i< size; i++){
+        for (int j=0; j < size; j++){
+            matrixProduct[i][j] = 0;
+            for (int k=0; k < size; k++){
+                matrixProduct[i][j] += matrixA[i][k] * matrixB[k][j];
+            }
+        }
+    }
+    for (int i =0; i < size; i++){
+        for (int j=0; j < size; j++){
+            // std::cout<< matrixProduct[i][j] << " ";
+            printf("%3d ", matrixProduct[i][j]);
+        }
+        std::cout << "\n";
+    }
+    std::cout << "Matrix Difference (A - B):\n";
+    int matrixDifference[MAX_SIZE][MAX_SIZE];
+
+    for (int i =0; i < size; i++){
+        for (int i = 0; i < size; i++){
+            for (int j=0; j< size; j++){
+                matrixDifference[i][j] = matrixA[i][j] - matrixB[i][j];
+            }
+        }
+        for (int j=0; j < size; j++){
+            std::cout<< matrixDifference[i][j] << " ";
+        }
+        std::cout << "\n";
+    }
+    return 0;
+}
---
+++ makefile
@@ -0,0 +1,4 @@
+main: Matrix.cpp
+	g++ -o main Matrix.cpp
+clean:
+	rm -f main Matrix.o
